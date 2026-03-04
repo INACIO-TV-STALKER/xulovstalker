6b2deb496f10129dc8a0c71805a3b72e91548999
Xulovski <vscgambuzinos@gmail.com>
2026-03-04 20:48:53 +0000

    Fix final: getStreams dentro do objeto

--- addon.cjs
+++ addon.cjs
@@ -114,7 +114,7 @@ async getCatalog(type, id, extra, configBase64) {
     } catch (e) {
         return { metas: [] };
     }
-}
+},
     async getStreams(type, id, configBase64) {
         const parts = id.split(":");
         const listIdx = parseInt(parts[1]);
